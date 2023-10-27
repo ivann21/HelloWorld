c45f3816776cc25ab7f36fda020e21557174790f
Ivan <icastellano2106@gmail.com>
2023-10-27 10:25:48 +0200

    calculadora

--- AndroidStudioProjects/HelloWorld/app/src/main/java/com/example/calculatortable/MainActivity.kt
+++ AndroidStudioProjects/HelloWorld/app/src/main/java/com/example/calculatortable/MainActivity.kt
@@ -2,12 +2,30 @@ package com.example.calculatortable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calculatortable.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        binding.acButton.setOnClickListener {
+            binding.operationsTextView.text = ""
+        }
 
+        binding.oneButton.setOnClickListener { _ ->
+            addTextToOperations("1")
+        }
+        binding.twoButton.setOnClickListener { _ ->
+            addTextToOperations("2")
+        }
+
+    }
+    fun addTextToOperations(v: String){
+        binding.operationsTextView.text =
+            binding.operationsTextView.text.toString() + v
     }
 }
