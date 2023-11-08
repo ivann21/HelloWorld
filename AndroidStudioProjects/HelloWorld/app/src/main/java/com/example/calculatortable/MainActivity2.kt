fc2f603b8d03a58aa1c53791e1d9f676b7727cc5
Ivan <icastellano2106@gmail.com>
2023-11-08 10:04:45 +0100

    descripcion

--- AndroidStudioProjects/HelloWorld/app/src/main/java/com/example/calculatortable/MainActivity2.kt
+++ AndroidStudioProjects/HelloWorld/app/src/main/java/com/example/calculatortable/MainActivity2.kt
@@ -2,6 +2,7 @@ package com.example.calculatortable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.calculatortable.databinding.ActivityMain2Binding
 import com.example.calculatortable.databinding.ActivityMainBinding
 
@@ -20,6 +21,19 @@ class MainActivity2 : AppCompatActivity() {
             binding.textView3.text = "Activado"
             else
                 binding.textView3.text = "Desactivado"
+        }
+        binding.helloButton.setOnClickListener {
+            when(binding.sexo.checkedRadioButtonId){
+                binding.male.id ->{
+                    Toast.makeText(this,"Sexo:masculino",Toast.LENGTH_SHORT).show()
+                }
+                binding.female.id ->{
+                    Toast.makeText(this,"Sexo:femenino",Toast.LENGTH_SHORT).show()
+                }
+                binding.nonBinary.id ->{
+                    Toast.makeText(this,"Sexo:no binario",Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
